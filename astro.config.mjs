c039a326495fee21f5f23ee4d29b19d7e3a28a1a
Ben Swift <ben@benswift.me>
2026-03-04 08:56:33 +1100

    configure remark-smartypants with oldschool dashes (--- for em dash)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,15 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import svelte from "@astrojs/svelte";
+import remarkSmartypants from "remark-smartypants";
 
 export default defineConfig({
   site: "https://cyberneticstudio.xyz",
   integrations: [svelte()],
+  markdown: {
+    smartypants: false,
+    remarkPlugins: [
+      /** @type {any} */ ([remarkSmartypants, { dashes: "oldschool" }]),
+    ],
+  },
 });
